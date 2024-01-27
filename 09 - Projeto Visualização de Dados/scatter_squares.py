b04af2a5fa32f26dfecd9382dd59c43f5fad664b
neifreitas <neijfreitas@gmail.com>
2024-01-27 12:45:54 -0300

    committed

--- 09 - Projeto Visualização de Dados/scatter_squares.py
+++ 09 - Projeto Visualização de Dados/scatter_squares.py
@@ -1,10 +1,6 @@
 from matplotlib import pyplot as plt
 
-# Define valores do eixo X
-input_values = [1,2,3,4,5]
-
-squares = [1,4,9,16,25]
-plt.plot(input_values, squares, linewidth=5)
+plt.scatter(2, 4, s=100)
 
 # Define o título do gráfico e nomeia os eixos
 plt.title("Square Numbers", fontsize=24)
@@ -12,6 +8,7 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
 # Define o tamanho dos rótulos das marcações
-plt.tick_params(axis='both', labelsize=14)
+plt.tick_params(axis="both", which='major', labelsize=14)
 
 plt.show()
+
