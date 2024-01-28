77b3ec52d4451a6937b5306d68cbf6c31ab4ccb7
neifreitas <neijfreitas@gmail.com>
2024-01-28 13:44:07 -0300

    Comitted

---
+++ 09 - Projeto Visualização de Dados/rw_visual.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Continua criando novos passeios enquanto o programa estiver ativo
+while True:
+    # Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    plt.scatter(rw.x_values, rw.y_values, s=5)
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
