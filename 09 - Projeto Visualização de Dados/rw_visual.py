1aef4e00f181b6fbcfda781a9c8a0603229cbbff
neifreitas <neijfreitas@gmail.com>
2024-01-28 14:08:30 -0300

    Comitted

--- 09 - Projeto Visualização de Dados/rw_visual.py
+++ 09 - Projeto Visualização de Dados/rw_visual.py
@@ -4,10 +4,16 @@ from random_walk import RandomWalk
 # Continua criando novos passeios enquanto o programa estiver ativo
 while True:
     # Cria um passeio aleatório e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(10000)
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=5)
+    # Versão original
+    # plt.scatter(rw.x_values, rw.y_values, s=5)
+    # plt.show()
+    
+    # Versão com cmap
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.RdPu, edgecolors='none', s=5)
     plt.show()
     
     keep_running = input("Make another walk? (y/n): ")
